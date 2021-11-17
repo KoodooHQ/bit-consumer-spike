4ad81e5619b9dbc0e0a9116ebe3db94569ce30ba
Thomas Moore <thomasmoore@Thomass-MacBook-Pro.local>
2021-11-17 10:00:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@tommoore96/bit-spike.ui.button";
+import { ThemeProvider } from "@tommoore96/bit-spike.ui.theme-provider";
+import { ErrorMessage } from "@tommoore96/bit-spike.ui.error-message";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ThemeProvider theme="koodooTheme">
+          <div>
+            <Button>Bit Button</Button>
+            <ErrorMessage>Ooops</ErrorMessage>
+          </div>
+        </ThemeProvider>
       </header>
     </div>
   );
